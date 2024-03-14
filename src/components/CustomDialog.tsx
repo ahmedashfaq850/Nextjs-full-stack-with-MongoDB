8683f5cc065156f5d97ecf8a3290b8ef627e54d9
Ahmed Ashfaq <74646219+ahmedashfaq850@users.noreply.github.com>
2024-03-14 20:10:15 +0500

    final build

--- src/components/CustomDialog.tsx
+++ src/components/CustomDialog.tsx
@@ -27,20 +27,6 @@ const CustomDialog = ({ id }:any) => {
               Make changes to your User Data here. Click save when you're done.
             </DialogDescription>
           </DialogHeader>
-          {/* <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Username
-              </Label>
-              <Input id="username" value="@peduarte" className="col-span-3" />
-            </div>
-          </div> */}
           <CommonForm text='Save Changes'  method='PUT' url='http://localhost:3000/api/edit-user' id={id} toastMessage='User Updated Successfully' apiError='Failed to update user' defaultValue = {data} />
           <DialogFooter>
             <Button type="submit">Save changes</Button>

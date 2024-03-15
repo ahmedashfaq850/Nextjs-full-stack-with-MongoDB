f831546ec7acb5c3a8ebbe52f4f3a495416b3f10
Ahmed Ashfaq <74646219+ahmedashfaq850@users.noreply.github.com>
2024-03-15 05:52:27 +0500

    final build

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,14 +1,22 @@
 import { User } from "@/models/user.model";
 import { NextResponse } from "next/server";
+
 export async function GET() {
   try {
     const usersData = await User.find({});
     if (usersData.length > 0) {
       return NextResponse.json(usersData);
     } else {
-      return NextResponse.json({ message: "No Users Found" });
+      return NextResponse.json(
+        "No Users Found",
+        { status: 404 }
+      );
     }
   } catch (err) {
     console.error("Error in Fetching", err);
+    return NextResponse.json(
+      "Internal Server Error",
+      { status: 500 }
+    );
   }
 }
